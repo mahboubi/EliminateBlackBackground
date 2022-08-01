4fd7b91a08bce8361bc51e489a041a3138309219
ali.mahboubiv <ali.mahboubiv@emiratespost.ae>
2022-08-01 10:42:00 +0400

    add source

---
+++ FormCrop.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WinFormsCropApp
+{
+    partial class FormCrop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.btnCropImage = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBoxCrop = new System.Windows.Forms.PictureBox();
+            this.checkBoxGama = new System.Windows.Forms.CheckBox();
+            this.checkBoxRotate = new System.Windows.Forms.CheckBox();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.btnOcr = new System.Windows.Forms.Button();
+            this.textOcr = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCrop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(395, 22);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(179, 59);
+            this.btnLoadImage.TabIndex = 0;
+            this.btnLoadImage.Text = "Load Image";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.EnabledChanged += new System.EventHandler(this.btnLoadImage_EnabledChanged);
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // btnCropImage
+            // 
+            this.btnCropImage.Enabled = false;
+            this.btnCropImage.Location = new System.Drawing.Point(753, 22);
+            this.btnCropImage.Name = "btnCropImage";
+            this.btnCropImage.Size = new System.Drawing.Size(174, 59);
+            this.btnCropImage.TabIndex = 1;
+            this.btnCropImage.Text = "Crop Image";
+            this.btnCropImage.UseVisualStyleBackColor = true;
+            this.btnCropImage.EnabledChanged += new System.EventHandler(this.btnCropImage_EnabledChanged);
+            this.btnCropImage.Click += new System.EventHandler(this.btnCropImage_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblFileName.Location = new System.Drawing.Point(29, 22);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(360, 58);
+            this.lblFileName.TabIndex = 2;
+            this.lblFileName.Text = "...";
+            this.lblFileName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblFileName.TextChanged += new System.EventHandler(this.lblFileName_TextChanged);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(27, 105);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(595, 540);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            // 
+            // pictureBoxCrop
+            // 
+            this.pictureBoxCrop.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxCrop.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxCrop.Location = new System.Drawing.Point(628, 105);
+            this.pictureBoxCrop.Name = "pictureBoxCrop";
+            this.pictureBoxCrop.Size = new System.Drawing.Size(631, 540);
+            this.pictureBoxCrop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxCrop.TabIndex = 4;
+            this.pictureBoxCrop.TabStop = false;
+            // 
+            // checkBoxGama
+            // 
+            this.checkBoxGama.AutoSize = true;
+            this.checkBoxGama.Location = new System.Drawing.Point(604, 3);
+            this.checkBoxGama.Name = "checkBoxGama";
+            this.checkBoxGama.Size = new System.Drawing.Size(143, 29);
+            this.checkBoxGama.TabIndex = 5;
+            this.checkBoxGama.Text = "Gamma Filter";
+            this.checkBoxGama.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxRotate
+            // 
+            this.checkBoxRotate.AutoSize = true;
+            this.checkBoxRotate.Location = new System.Drawing.Point(604, 38);
+            this.checkBoxRotate.Name = "checkBoxRotate";
+            this.checkBoxRotate.Size = new System.Drawing.Size(89, 29);
+            this.checkBoxRotate.TabIndex = 9;
+            this.checkBoxRotate.Text = "Rotate";
+            this.checkBoxRotate.UseVisualStyleBackColor = true;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Location = new System.Drawing.Point(1237, 39);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(22, 25);
+            this.labelProgress.TabIndex = 10;
+            this.labelProgress.Text = "0";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // btnOcr
+            // 
+            this.btnOcr.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOcr.Enabled = false;
+            this.btnOcr.Location = new System.Drawing.Point(1552, 22);
+            this.btnOcr.Name = "btnOcr";
+            this.btnOcr.Size = new System.Drawing.Size(177, 59);
+            this.btnOcr.TabIndex = 11;
+            this.btnOcr.Text = "OCR Image";
+            this.btnOcr.UseVisualStyleBackColor = true;
+            this.btnOcr.Click += new System.EventHandler(this.btnOcr_Click);
+            // 
+            // textOcr
+            // 
+            this.textOcr.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textOcr.Location = new System.Drawing.Point(1265, 105);
+            this.textOcr.Name = "textOcr";
+            this.textOcr.ReadOnly = true;
+            this.textOcr.Size = new System.Drawing.Size(464, 540);
+            this.textOcr.TabIndex = 12;
+            this.textOcr.Text = "";
+            // 
+            // FormCrop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1741, 657);
+            this.Controls.Add(this.textOcr);
+            this.Controls.Add(this.btnOcr);
+            this.Controls.Add(this.labelProgress);
+            this.Controls.Add(this.checkBoxRotate);
+            this.Controls.Add(this.checkBoxGama);
+            this.Controls.Add(this.pictureBoxCrop);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.btnCropImage);
+            this.Controls.Add(this.btnLoadImage);
+            this.Name = "FormCrop";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Crop Image...";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCrop)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnLoadImage;
+        private Button btnCropImage;
+        private Label lblFileName;
+        private OpenFileDialog openFileDialog;
+        private PictureBox pictureBox;
+        private PictureBox pictureBoxCrop;
+        private CheckBox checkBoxGama;
+        private CheckBox checkBoxRotate;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private Label labelProgress;
+        private System.Windows.Forms.Timer timer;
+        private Button btnOcr;
+        private RichTextBox textOcr;
+    }
+}
---
+++ FormCrop.cs
@@ -0,0 +1,304 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.ComponentModel;
+using System.Data;
+using Emgu.CV.IntensityTransform;
+using Emgu.CV.OCR;
+
+
+namespace WinFormsCropApp
+{
+    public partial class FormCrop : Form
+    {
+
+        int counter = 1;
+        string croppedFileName = string.Empty;
+        public FormCrop()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            var result = openFileDialog.ShowDialog(this);
+            lblFileName.Text = openFileDialog.FileName;
+            pictureBox.Image = Image.FromFile(lblFileName.Text);
+        }
+
+        private void btnCropImage_Click(object sender, EventArgs e)
+        {
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+            backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
+            btnCropImage.Enabled = false;
+            btnLoadImage.Enabled = false;
+            timer.Enabled = true;
+            backgroundWorker.RunWorkerAsync();          
+        }
+
+        private void BackgroundWorker_ProgressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+           
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
+        {
+            btnCropImage.Enabled = true;
+            btnLoadImage.Enabled = true;
+            counter = 1;
+            timer.Enabled = false;
+            labelProgress.Text = "0";
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {        
+            ProcessImage(lblFileName.Text);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            labelProgress.Text = counter.ToString();
+            counter++;
+        }
+
+        private void btnLoadImage_EnabledChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCropImage_EnabledChanged(object sender, EventArgs e)
+        {
+            if (!btnCropImage.Enabled)
+                btnCropImage.Text = "Crop Image...";
+            else
+                btnCropImage.Text = "Crop Image";
+        }
+
+        private void lblFileName_TextChanged(object sender, EventArgs e)
+        {
+            btnCropImage.Enabled = true;
+            btnOcr.Enabled = true;
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        void ProcessImage(string imagePath)
+        {
+            var fileInfo = new FileInfo(imagePath);
+            var imagesPath = fileInfo.DirectoryName;
+            var filename = fileInfo.Name.Replace(fileInfo.Extension, "");
+            var extention = fileInfo.Extension;
+            var imagepath = imagesPath + @"\" + filename + extention;
+
+            Mat img = new Mat(imagepath);
+            Mat gama = new Mat();
+
+            if (checkBoxGama.Checked)
+                IntensityTransformInvoke.GammaCorrection(img, img, 0.5F);
+
+          
+            using (UMat gray = new UMat())
+            using (UMat cannyEdges = new UMat())
+            using (Mat lineImage = new Mat(img.Size, DepthType.Cv8U, 3)) //image to drtaw lines on
+            {
+                //Convert the image to grayscale and filter out the noise
+                
+                UMat threshold = new UMat();
+                CvInvoke.CvtColor(img, gray, ColorConversion.Bgr2Gray);
+                
+                double cannyThreshold = 180.0;
+                
+                CvInvoke.GaussianBlur(gray, gray, new Size(3, 3), 1);
+
+                CvInvoke.Threshold(gray, gray, 210, 100, ThresholdType.Binary);
+
+                #region Canny and edge detection
+                double cannyThresholdLinking = 120.0;
+                CvInvoke.Canny(gray, cannyEdges, cannyThreshold, cannyThresholdLinking);
+
+                LineSegment2D[] lines = CvInvoke.HoughLinesP(
+                    cannyEdges,
+                    1, //Distance resolution in pixel-related units
+                    Math.PI / 45.0, //Angle resolution measured in radians.
+                    20, //threshold
+                    50, //min Line width
+                    10); //gap between lines
+
+                List<DestPoint> dests00 = new List<DestPoint>();
+                List<DestPoint> dests0x = new List<DestPoint>();
+
+                Image image = Bitmap.FromFile(imagepath);
+
+                foreach (var line in lines)
+                {
+                    
+                    var dest1 = Math.Sqrt((line.P1.Y * line.P1.Y));
+                    dests00.Add(new DestPoint() { point = new Point() { X = (int)line.P1.X, Y = (int)line.P1.Y }, dest = dest1, Ditection = line.Direction });
+
+                    var dest2 = Math.Sqrt((line.P2.Y * line.P2.Y));
+                    dests00.Add(new DestPoint() { point = new Point() { X = (int)line.P1.X, Y = (int)line.P1.Y }, dest = dest2, Ditection = line.Direction });
+
+                    dest1 = Math.Sqrt(((image.Width - line.P1.X) * (image.Width - line.P1.X)));
+                    dests0x.Add(new DestPoint() { point = new Point() { X = (int)line.P1.X, Y = (int)line.P1.Y }, dest = dest1, Ditection = line.Direction });
+
+                    dest2 = Math.Sqrt(((image.Width - line.P2.X) * (image.Width - line.P2.X)));
+                    dests0x.Add(new DestPoint() { point = new Point() { X = (int)line.P1.X, Y = (int)line.P1.Y }, dest = dest2, Ditection = line.Direction });
+
+                }
+
+                var closePoint = dests00.OrderBy(x => x.dest).First();
+                var farPoint = dests00.OrderByDescending(x => x.dest).First();
+
+                var closePointOX = dests0x.OrderBy(x => x.dest).First();
+                var farPoint0X = dests0x.OrderByDescending(x => x.dest).First();
+
+                var degClose = Math.Atan2((closePointOX.point.Y - closePoint.point.Y), (closePointOX.point.X - closePoint.point.X)) * (180 / Math.PI);
+
+                var degFar = Math.Atan2((farPoint0X.point.Y - farPoint.point.Y), (farPoint0X.point.X - farPoint.point.X)) * (180 / Math.PI);
+
+                double destWidth = 0;
+                double destHeight = 0;
+
+                if ((degClose > 0 & (degFar < 0 || degFar == 0)))
+                {
+                    destWidth = Math.Sqrt(Math.Pow((closePointOX.point.X - farPoint0X.point.X), 2) + Math.Pow((closePoint.point.Y - closePoint.point.Y), 2));
+
+                    destHeight = Math.Sqrt(Math.Pow((farPoint0X.point.X - farPoint0X.point.X), 2) + Math.Pow((farPoint.point.Y - closePoint.point.Y), 2));
+                }
+                else
+
+                if (degClose < 0 & degFar > 0)
+                {
+                    destWidth = Math.Sqrt(Math.Pow((farPoint.point.X - closePoint.point.X), 2) + Math.Pow((closePointOX.point.Y - closePointOX.point.Y), 2));
+
+                    destHeight = Math.Sqrt(Math.Pow((closePoint.point.X - closePoint.point.X), 2) + Math.Pow((farPoint0X.point.Y - closePoint.point.Y), 2));
+                }
+
+                else
+
+                if (degClose < 0 & (degFar < 0 || degFar == 0))
+                {
+                    destWidth = Math.Sqrt(Math.Pow((closePointOX.point.X - closePoint.point.X), 2) + Math.Pow((closePointOX.point.Y - closePointOX.point.Y), 2));
+
+                    destHeight = Math.Sqrt(Math.Pow((closePoint.point.X - closePoint.point.X), 2) + Math.Pow((farPoint0X.point.Y - closePoint.point.Y), 2));
+                }
+
+                else
+
+                if (degClose > 0 & degFar > 0)
+                {
+                    destWidth = Math.Sqrt(Math.Pow((closePointOX.point.X - closePoint.point.X), 2) + Math.Pow((closePointOX.point.Y - closePointOX.point.Y), 2));
+
+                    destHeight = Math.Sqrt(Math.Pow((closePoint.point.X - closePoint.point.X), 2) + Math.Pow((farPoint0X.point.Y - closePoint.point.Y), 2));
+                }
+
+                destHeight = destHeight + 100;
+                destWidth = destWidth + 100;
+                closePoint.point = new Point() { X = closePoint.point.X - 50, Y = closePoint.point.Y - 50 };
+                closePointOX.point = new Point() { X = closePointOX.point.X - 50, Y = closePointOX.point.Y - 50 };
+                farPoint.point = new Point() { X = farPoint.point.X - 50, Y = farPoint.point.Y - 50 };
+                farPoint0X.point = new Point() { X = farPoint0X.point.X - 50, Y = farPoint0X.point.Y - 50 };
+                
+                Image destinationImage = new Bitmap((int)destWidth, (int)destHeight);
+
+                using (Graphics g = Graphics.FromImage(destinationImage))
+                {
+                    float x = 0.0F;
+                    float y = 0.0F;
+
+                    RectangleF srcRect;
+                    if (degClose > 0 & degFar < 0)
+                    {
+                        srcRect = new RectangleF(farPoint0X.point.X, closePoint.point.Y, (float)destWidth, (float)destHeight);
+                        g.DrawImage(image, x, y, srcRect, GraphicsUnit.Pixel);
+                    }
+                    else
+                    if (degClose > 0 & degFar > 0)
+                    {
+                        srcRect = new RectangleF(closePoint.point.X, closePointOX.point.Y, (float)destWidth, (float)destHeight);
+                        g.DrawImage(image, x, y, srcRect, GraphicsUnit.Pixel);
+                    }
+                    else
+                     if (degClose < 0 & degFar > 0)
+                    {
+                        srcRect = new RectangleF(closePoint.point.X, closePointOX.point.Y, (float)destWidth, (float)destHeight);
+                        g.DrawImage(image, x, y, srcRect, GraphicsUnit.Pixel);
+                    }
+                    else
+                     if (degClose < 0 & degFar < 0)
+                    {                       
+                        srcRect = new RectangleF(closePoint.point.X, closePointOX.point.Y, (float)destWidth, (float)destHeight);
+                        g.DrawImage(image, x, y, srcRect, GraphicsUnit.Pixel);
+                    }
+                    else
+                    if (degClose < 0 & degFar == 0)
+                    {
+                        srcRect = new RectangleF(closePoint.point.X, closePointOX.point.Y, (float)destWidth, (float)destHeight);
+                        g.DrawImage(image, x, y, srcRect, GraphicsUnit.Pixel);
+                    }
+                    else
+                    if (degClose > 0 & degFar == 0)
+                    {
+                        srcRect = new RectangleF(farPoint0X.point.X, closePoint.point.Y, (float)destWidth, (float)destHeight);
+                        g.DrawImage(image, x, y, srcRect, GraphicsUnit.Pixel);
+                    }
+
+                    if (checkBoxRotate.Checked)
+                        destinationImage.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                                       
+                    g.DrawString("@Cropped Image", new Font(new FontFamily("Tahoma"), 50, FontStyle.Bold), Brushes.Red, new PointF() { X = 0, Y = 0 });
+
+                   
+                    croppedFileName = imagesPath + @"\" + filename + "_cropped" + extention;
+                    destinationImage.Save(croppedFileName);
+                    pictureBoxCrop.Image = destinationImage;
+
+                  
+                }
+                #endregion
+
+            }
+        }
+
+     
+      
+        private void btnOcr_Click(object sender, EventArgs e)
+        {
+            string result = string.Empty;
+            Task.Run(() =>
+            {
+               
+                using (var img =  new Mat(croppedFileName))
+                {                 
+                    string tessdata = @"C:\.....\tessdata";
+                    using (var ocrProvider = new Tesseract(tessdata, "eng", OcrEngineMode.TesseractOnly))  //TesseractCubeCombined
+                    {
+                        ocrProvider.SetImage(img); //Recognize
+                        result = ocrProvider.GetUTF8Text().TrimEnd(); //GetText
+                       
+                        textOcr.Invoke((MethodInvoker)delegate
+                        {
+                            textOcr.Text = result;
+                        });
+                    }
+                }
+            });
+
+            
+        }
+    }
+
+  
+    public class DestPoint
+    {
+        public Point point { get; set; }
+
+        public double dest { get; set; }
+
+        public PointF Ditection { get; set; }
+    }
+}
